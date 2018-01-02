c3894aa377757d09a19bb71321ebba72080a9fc1
Santiago Gallego <santiago.gallego.rodrigo@gmail.com>
2018-01-02 10:25:59 +0100

    Minor fixes and improvements
    Change-Id: I84a8611bb8b5f3a6bee85e251008e82e85d4c44b

--- stock.py
+++ stock.py
@@ -27,9 +27,10 @@ for retry in range(1, 6):
     try:
         stock = DataReader('^IBEX',  'yahoo', datetime(2005, 1, 1), datetime.today())
     except:
-        print("RETRY #%d - Error reading from Yahoo Finance") % (retry)
+        print("RETRY #%d - Error reading from Yahoo Finance" % (retry))
         continue
     break
+
 
 # Build DataFrame from rolling averages
 rolling_5d = pd.rolling_mean(stock['Adj Close'], 5, min_periods=1)
@@ -49,33 +50,57 @@ rolling_averages = pd.DataFrame({
 # Generate figures
 plot_params = {
     'title': 'Rolling averages',
+    'linewidth': 2,
+    'solid_capstyle': 'round',
     'grid': True,
     'fontsize': 8,
     'colormap': cm.gist_rainbow
 }
 
-plt.figure()
+plt.figure(figsize=(32, 24), dpi=100)
 rolling_averages.plot(**plot_params)
-plt.savefig('image1.png')
+plt.savefig('image1.pdf', dpi=100)
 plt.close()
 
-plt.figure()
+plt.figure(figsize=(32, 24), dpi=100)
 rolling_averages[-250:].plot(**plot_params)
-plt.savefig('image2.png')
+plt.savefig('image2.pdf', dpi=100)
 plt.close()
 
-plt.figure()
+plt.figure(figsize=(32, 24), dpi=100)
 rolling_averages[-60:].plot(**plot_params)
-plt.savefig('image3.png')
+plt.savefig('image3.pdf', dpi=100)
 plt.close()
+
 
 # Send info to telegram bot
 formatted_date = datetime.today().strftime("%Y-%m-%d")
 subprocess.call("telegram-send -- '*********'", shell=True)
 subprocess.call("telegram-send -- 'ANALYSIS: %s'" % (formatted_date), shell=True)
 
+
+prev_close = stock['Adj Close'][-3]
 last_close = stock['Adj Close'][-2]
+diff_abs = last_close-prev_close
+diff_pct = 100.0 * (last_close-prev_close) / prev_close
+
 subprocess.call("telegram-send -- 'Close: %.2f'" % (last_close), shell=True)
+if (diff_abs > 0.0):
+    subprocess.call("telegram-send -- 'Diff: +%.2f (+%.2f%%)'" % (diff_abs, diff_pct), shell=True)
+else:
+    subprocess.call("telegram-send -- 'Diff: %.2f (%.2f%%)'" % (diff_abs, diff_pct), shell=True)
+
+open_val = stock['Open'][-2]
+high_val = stock['High'][-2]
+low_val = stock['Low'][-2]
+vol_val = stock['Volume'][-2]
+
+subprocess.call("telegram-send -- 'Op: %.2f'" % (open_val), shell=True)
+subprocess.call("telegram-send -- 'Hi: %.2f'" % (high_val), shell=True)
+subprocess.call("telegram-send -- 'Lo: %.2f'" % (low_val), shell=True)
+subprocess.call("telegram-send -- 'Volume: %.1f'" % (vol_val), shell=True)
+
+subprocess.call("telegram-send -- '---'" % (last_close), shell=True)
 
 trend = getTrendStr(last_close, rolling_5d[-2])
 subprocess.call("telegram-send -- '5d avg: %.2f (%s)'" % (rolling_5d[-2], trend), shell=True)
@@ -89,7 +114,8 @@ subprocess.call("telegram-send -- '60d avg: %.2f (%s)'" % (rolling_60d[-2], tren
 trend = getTrendStr(last_close, rolling_250d[-2])
 subprocess.call("telegram-send -- '250d avg: %.2f (%s)'" % (rolling_250d[-2], trend), shell=True)
 
-subprocess.call("telegram-send -i image1.png", shell=True)
-subprocess.call("telegram-send -i image2.png", shell=True)
-subprocess.call("telegram-send -i image3.png", shell=True)
+subprocess.call("telegram-send -- '---'" % (last_close), shell=True)
 
+subprocess.call("telegram-send -f image1.pdf image2.pdf image3.pdf", shell=True)
+
+subprocess.call("rm image1.pdf image2.pdf image3.pdf", shell=True)
