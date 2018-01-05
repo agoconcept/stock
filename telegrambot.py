c048dfe4dfb953c4c332e483ed042aa55571f48b
Santiago Gallego <santiago.gallego.rodrigo@gmail.com>
2018-01-05 12:38:50 +0100

    Add telegram bot
    Change-Id: I12e171a412e09d16c55f3c762b4c80a22850acce

---
+++ telegrambot.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+# Based on https://www.codementor.io/garethdwyer/building-a-telegram-bot-using-python-part-1-goi5fncay
+
+import json
+import requests
+import urllib
+import subprocess
+
+
+with open('token', 'r') as myfile:
+    TOKEN = myfile.read().replace('\n', '')
+
+URL = "https://api.telegram.org/bot%s/" % (TOKEN)
+
+
+def get_url(url):
+    response = requests.get(url)
+    # TODO: Exception handling
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=120"
+    if offset:
+        url += "&offset=%d" % (offset)
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+
+def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text=%s&chat_id=%d" % (text, chat_id)
+    get_url(url)
+
+
+def parse_all(updates):
+    for update in updates["result"]:
+        try:
+            if (update["message"]["text"] == "/report"):
+                subprocess.call("./stockReport", shell=True)
+            else:
+                text = update["message"]["text"]
+                chat = update["message"]["chat"]["id"]
+                send_message(text, chat)
+        except Exception as e:
+            print(e)
+
+
+def main():
+    last_update_id = None
+    while (True):
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            parse_all(updates)
+
+
+if __name__ == '__main__':
+    main()
